93f06f40fc9957616e22402cad25da5d04807bb4
dataneer <coyletheodorre@gmail.com>
2021-03-15 16:58:27 -0400

    Prob Calculator changes

--- Scientific_Computing_Python/Polygon_Area_Calculator/shape_calculator.py
+++ Scientific_Computing_Python/Polygon_Area_Calculator/shape_calculator.py
@@ -3,14 +3,19 @@ class Rectangle:
     #`width` and `height` attributes.
 
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
+        if type(width) == int and type(height) == int:
+            self.width = width
+            self.height = height
+        else:
+            raise TypeError("Width and height have to be integers.")
 
+    # Additionally, if an instance of a Rectangle is represented as a string,
+    # it should look like: `Rectangle(width=5, height=10)`
     def __repr__(self):
-        return f'Rectangle: Width: {self.width}\nHeight: {self.height}'
+        return f'Rectangle(width={self.width}, height={self.height})'
 
     # Method that reassigns width
-    def self(width, width_num):
+    def set_width(self, width_num):
         self.width = width_num
 
     # Method that reassigns height
@@ -30,20 +35,76 @@ class Rectangle:
         return diagonal
 
     def get_picture(self):
-        pass
+        # check if self.width or self.height is greater than 50
+        if self.width > 50 or self.height > 50:
+            return 'Too big for picture.'
+        else:
+            pic_width = self.width * '*'
+            # Append pic_width as a list item n times based on the value
+            # of self.height
+            picture_lst = [pic_width] * self.height
+            # Combine picture_lst into a string with new lines using .join()
+            picture_shape = '\n'.join(picture_lst)
+            return picture_shape
 
-    def get_amount_inside(self):
-        pass
+    # * `get_amount_inside`: Takes another shape (square or rectangle) as an
+    # argument. Returns the number of times the passed in shape could fit inside
+    # the shape (with no rotations). For instance, a rectangle with a width of
+    # 4 and a height of 8 could fit in two squares with sides of 4.
+    def get_amount_inside(self, shape):
+        # first d
+        if self.width > shape.width and self.height > shape.height:
+            # Use flat division to compare whole numbers
+            compare_w = self.width // shape.width
+            compare_h = self.height // shape.height
+            inside_shape = compare_w * compare_h
+            return inside_shape
+        else:
+            return "Shape too large."
 
     # Additionally, if an instance of a Rectangle is represented as a string,
     # it should look like: `Rectangle(width=5, height=10)`
     # print(f"Rectangle(width=5{self.width}, height={self.height})")
 
 
-class Square:
-    pass
+class Square(Rectangle):
+
+    def __init__(self, side):
+        if type(side) == int:
+            self.width = side
+            self.height = side
+        else:
+            raise TypeError("Side has to be a integer.")
+
+    # If an instance of a Square is represented as a string, it should
+    # look like: `Square(side=9)
+    def __repr__(self):
+        return f'Square(side={self.width})'
+
+    # The Square class should be able to access the Rectangle class methods but
+    # should also contain a `set_side` method.`
+    def set_side(self, side_num):
+        self.width = side_num
+        self.height = side_num
 
 
-y = Rectangle(5, 7)
-print(y)
-print('get_diagonal:', y.get_diagonal())
+
+
+
+rect = Rectangle(10, 5)
+print(rect.get_area())
+rect.set_height(3)
+print(rect.get_perimeter())
+print(rect)
+print(rect.get_picture())
+
+sq = Square(9)
+print(sq.get_area())
+sq.set_side(4)
+print(sq.get_diagonal())
+print(sq)
+print(sq.get_picture())
+
+rect.set_height(8)
+rect.set_width(16)
+print(rect.get_amount_inside(sq))
--- Scientific_Computing_Python/Probability_Calculator/prob_calculator.py
+++ Scientific_Computing_Python/Probability_Calculator/prob_calculator.py
@@ -2,7 +2,46 @@ import copy
 import random
 # Consider using the modules imported above.
 
+# First, create a `Hat` class in `prob_calculator.py`. The class should
+# take a variable number of arguments that specify the number of balls
+# of each color that are in the hat. For example, a class object could
+# be created in any of these ways:
+
+# hat1 = Hat(yellow=3, blue=2, green=6)
+# hat2 = Hat(red=5, orange=4)
+# hat3 = Hat(red=5, orange=4, black=1, blue=0, pink=2, striped=9)
+
 class Hat:
 
+    # Originally here I used an *args before I discovered **kwargs so
+    # I am going to write the differences betweent the two here for my future
+    # reference
+
+    # *args is used to pass non-key worded, variable-length arguments
+    # You can take in more arguments than the # of formal arguments that you
+    # previously defined with. Any extra arguements can be tacked onto your
+    # current formal paramaters (including zero extra arguments)
+
+    # **kwargs is used to pass keyworded, variable-length arguments
+    # One can thnk of wards being a dictionary that maps each keyword to the
+    # value that we pass alongside it. That's why when we iterate over the
+    # kwargs there doesn't seem to beany order in which they were printed out
+
+    # args: ('geeks', 'for', 'geeks')
+    # kwargs {'first': 'Geeks', 'mid': 'for', 'last': 'Geeks'}
+    def __init__(self,**balls):
+        self.balls = balls
+
+    def __repr__(self):
+        return f'Balls: {self.balls}'
+
+    # `expected_balls`: An object indicating the exact group of balls to
+    # attempt to draw from the hat for the experiment. For example, to determine
+    # the probability of drawing 2 blue balls and 1 red ball from the hat, set
+    # `expected_balls` to `{"blue":2, "red":1}`.
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    pass
+
+hat1 = Hat(yellow=3, blue=2, green=6)
+print(hat1)
